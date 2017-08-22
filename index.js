edecce1b00be942598f65260157e9c9078adee5d
Aaron Freidus <aaronf101@gmail.com>
2017-08-22 00:08:39 -0400

    added zoom controls and scale controls as a bonus.

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const SpeechRecognition = webkitSpeechRecognition;
 const SpeechGrammarList = webkitSpeechGrammarList;
 const SpeechRecognitionEvent = webkitSpeechRecognitionEvent;
+const numberMap = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
+    'twenty': 20
+}
+
 
 function voice ( config ) {
   // config will have:
   // Grammar, view (for controls), data for mapping of loc to lat long.
   // scale/extent?
+  let customGrammar = '#JSGF V1.0; grammar countries; public <word> = ' + config.words.join(' | ') + ' ;';
   this.recognition = new SpeechRecognition();
   this.speechRecognitionList = new SpeechGrammarList();
-  this.speechRecognitionList.addFromString( config.grammar, 1 );
+  this.speechRecognitionList.addFromString( customGrammar, 1 );
+
 
   this.view = config.view || {};
   this.data = config.data || {};
@@ -20,11 +46,38 @@ function voice ( config ) {
   this.recognition.interimResults = false;
   this.recognition.maxAlternatives = 1;
 
+  let scaleCommands = '#JSGF V1.0; grammar scaleCommands; public <scale> = ' + [...Array(20).keys()].map((item) => 'scale ' + item.toString()).join(' | ') + ' ;';
+  let zoomCommands = '#JSGF V1.0; grammar zoomCommands; public <zoom> = ' + [...Array(20).keys()].map((item) => 'zoom ' + item.toString()).join(' | ') + ' ;';
+
+  this.speechRecognitionList.addFromString(scaleCommands, 1);
+  this.speechRecognitionList.addFromString(zoomCommands, 1)
+
+
   this.recognition.onresult = e => {
-    console.log(e.results)
-     var location = e.results[ e.results.length - 1 ][ 0 ].transcript;
-     let coordinates = this.getLatLon( location );
-     this.setMapPosition( coordinates );
+     let result = e.results[ e.results.length - 1 ][0],
+         zoomCheckIndex = result.transcript.toLowerCase().indexOf('zoom'),
+         scaleCheckIndex = result.transcript.toLowerCase().indexOf('scale');
+
+     console.log(result, zoomCheckIndex, scaleCheckIndex)
+     if(zoomCheckIndex > -1){
+        let zoom = result.transcript.toLowerCase().slice(zoomCheckIndex + 5);
+        console.log('zoombefore', zoom)
+        if(isNaN(Number(zoom))) zoom = numberMap[ zoom ];
+        console.log(zoom, numberMap)
+
+        this.view.goTo({ zoom })
+        console.log(zoom)
+     } else if(scaleCheckIndex > -1){
+        let scale = result.transcript.toLowerCase().slice(scaleCheckIndex + 5);
+        if(isNaN(Number(scale))) scale = numberMap[ scale ];
+        console.log(scale)
+        this.view.goTo({ scale })
+     } else {
+        let location = e.results[ e.results.length - 1 ][ 0 ].transcript;
+        let coordinates = this.getLatLon( location );
+        this.setMapPosition( coordinates );
+     }
+
   }
   // Keeps the listner alive if continuous is true, otherwise let's it die
   this.recognition.onend = e => {
@@ -52,7 +105,6 @@ function voice ( config ) {
   this.start = () => {
     this.recognition.start();
   } 
-
 
   return this;
 }
